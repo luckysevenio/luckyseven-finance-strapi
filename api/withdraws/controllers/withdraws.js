2be60b88b9b58e5c06cbfa2594d8249c4e7f5089
Matías Barrios <matias.barriosn@gmail.com>
2021-04-11 21:15:06 -0400

    feat: deposits by date and organization

--- api/withdraws/controllers/withdraws.js
+++ api/withdraws/controllers/withdraws.js
@@ -4,12 +4,42 @@
  */
 
 const Fintoc = require('fintoc');
+const moment = require('moment');
 
-const client = new Fintoc('api_key');
+const { FINTOC_API_KEY, FINTOC_BANCO_CHILE_LINK } = process.env;
+const client = new Fintoc(FINTOC_API_KEY);
 
 module.exports = {
-  getAll: async (ctx) => {
-    console.log(ctx);
-    console.log(process.env.MONGO_URI);
+  findByDate: async (ctx) => {
+    const { year, month } = ctx.params;
+    const { organization } = ctx.query;
+    const since = moment().set({ year, month }).startOf('month').toISOString();
+    const until = moment().set({ year, month }).endOf('month').toISOString();
+    const bancoChileLink = await client.getLink(FINTOC_BANCO_CHILE_LINK);
+    const bancoChileAccount = bancoChileLink.find({ type_: 'checking_account' });
+    const movements = await bancoChileAccount.getMovements({ since, until, per_page: 300 });
+    const regexes = await strapi.query('deposit-regex').find((organization && { organization }) || undefined);
+    const filteredMovements = movements.reduce(
+      (r, movement) => {
+        if (regexes.map((regex) => regex.regex).includes(movement.description)) {
+          r.push(
+            {
+              ...movement,
+              organization: organization || regexes.find(
+                (regex) => regex.regex === movement.description,
+              ).organization,
+            },
+          );
+        }
+        return r;
+      }, [],
+    );
+    return filteredMovements;
+  },
+  getMonthResult: async (ctx) => {
+    const filteredMovements = await strapi.controllers.withdraws.findByDate(ctx);
+    return filteredMovements.reduce((r, movement) => ({
+      amount: r.amount + movement.amount,
+    }), { amount: null });
   },
 };
