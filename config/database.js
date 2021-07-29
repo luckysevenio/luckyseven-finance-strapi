7d39b3d6c35bf0b49350bfe085943db7f857c464
Matías Barrios <matias.barriosn@gmail.com>
2021-07-29 18:29:21 -0400

    feat: mongo connection updated

--- config/database.js
+++ config/database.js
@@ -4,13 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('MONGO_URI'),
+        host: env('DATABASE_HOST', ''),
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
+        database: env('DATABASE_NAME', 'l7finance'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
         ssl: env.bool('DATABASE_SSL', false),
       },
     },
